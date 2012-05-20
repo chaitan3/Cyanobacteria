02c2bf2391575c86135345034f6936f40235b20a
Chaitanya Talnikar <chaitanya.talnikar@iitb.ac.in>
2012-05-20 15:12:50 +0530

    microRNA fasta files

--- microRNA/make.py
+++ microRNA/make.py
@@ -1,18 +1,30 @@
 from Bio import SeqIO
 
-utr_size = 180
-window_size = 30
+utr5_size = 47
+utr3_size = 63
 
-gene = 413873
-utr = gene-utr_size
-search = gene-600
+
+start = 413873
+end = 414955
+
+
+
 
 r = SeqIO.parse("../Genome/circular-elongatus.fasta",'fasta')
 s = r.next()
-mirnas = []
-for i in range(0, 151, 10):
-	mirnas.append(s[utr+i:utr+i+window_size])
-SeqIO.write(mirnas,'utr.fasta','fasta')
-SeqIO.write([s[search:gene]],'search.fasta','fasta')
+
+reg = [
+#3'UTR
+s[415031:415062],
+s[415078:415122],
+s[415151:415177],
+s[415217:415253],
+s[415291:415310],
+s[415352:415377]
+
+]
+SeqIO.write(reg,'db.fasta','fasta')
+
+SeqIO.write([s[end+utr3_size:end+500]],'search.fasta','fasta')
 
 
