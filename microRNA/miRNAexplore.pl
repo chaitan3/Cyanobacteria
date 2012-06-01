21e2c69f7b934fbd48a368307eb73b5df8ee285b
Chaitanya Talnikar <chaitanya.talnikar@iitb.ac.in>
2012-06-01 16:17:49 +0530

    miRNA

--- microRNA/miRNAexplore.pl
+++ microRNA/miRNAexplore.pl
@@ -5,14 +5,20 @@ $gene = $gene_in->next_seq();
 $seq_in = Bio::SeqIO->new(-file=>'miRNA7.fasta');
 
 $seq = $seq_in->next_seq();
+$c=0;
 while(my $seq = $seq_in->next_seq()) {
+	$c++;
+	mkdir $c;
+	chdir $c;
 	open (MYFILE, '>tmp.fasta');
 	$seq1=$seq->seq;
 	$seq2=$gene->seq;
 	print MYFILE "$seq1\&$seq2\n";
 	close (MYFILE); 
 	my $r=`RNAcofold -p <tmp.fasta`;
-	print $r;
+	open (MY, '>rna.out');
+	print MY $r;
+	chdir '..';
 }
 
 
