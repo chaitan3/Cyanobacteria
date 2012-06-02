7008a5ad10da721dc0635c644f71b58fee2e02b1
Chaitanya Talnikar <chaitanya.talnikar@iitb.ac.in>
2012-06-03 00:50:34 +0530

    miRNA

--- Deep Sequencing/Data/miRNAtilingraw.py
+++ Deep Sequencing/Data/miRNAtilingraw.py
@@ -1,5 +1,5 @@
-
-import subprocess
+from math import log
+import commands
 from Bio import SeqIO
 
 neg = 229169
@@ -7,11 +7,12 @@ gene = 34489
 time = [60, 68, 72, 80]
 base = '/home/chaitan3/GSE-Raw/GSM7242'
 num = 28
+seqio= open("../../microRNA/miRNA7.fasta")
+acc = []
 
 c = 0
-for seq in SeqIO.parse("../../microRNA/miRNA7.fasta", "fasta"):
+for seq in SeqIO.parse(seqio, "fasta"):
 	c += 1
-	print seq.id
 	s = seq.id.split(',')
 	s[0] = s[0][1:]
 	strand = int(s[2])
@@ -23,15 +24,27 @@ for seq in SeqIO.parse("../../microRNA/miRNA7.fasta", "fasta"):
 	else:
 		start = int(s[1]) - int(s[4])
 		end = int(s[1]) - int(s[3])
-		probe = start/12 + neg
+		probe = start/12# + neg
 		pref = 'R'
 	print probe, c
-	
+	fg = []
+	bg = []
+#	probe = 34489
 	for i in range(0, len(time)):
-		filename = base + str(num + i) + '_' + pref + str(time[i])
+		filename = base + str(num + i) + '_' + pref + str(time[i]) + '.gpr'
 		row = pref + str(probe)
-		print filename
+		out = commands.getoutput('grep ' + row + ' ' + filename)
+		fields = out.split('\t')
+		#4-Name, 9-F635 Mean, 14-B635 Mean, 44-Log Ratio, 47-F-B635,48-F-B532
+		fg.append(float(fields[48]))
+		bg.append(float(fields[47]))
+	if min(fg) > 100:
+		print "qualifies"
+		acc.append(c)
+		for i in range(0, len(time)):
+			print fg[i], bg[i], log(fg[i]/bg[i],2)
+print acc
 		
-		print subprocess.check_output('grep ' + row + ' ' + filename)
+	
 	
 	
