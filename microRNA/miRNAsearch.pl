42e8232b62d58169b7d73fff61420d159e43d994
Chaitanya Talnikar <chaitanya.talnikar@iitb.ac.in>
2012-06-03 23:48:00 +0530

    db

--- microRNA/miRNAsearch.pl
+++ microRNA/miRNAsearch.pl
@@ -12,7 +12,7 @@ else {
 }
 $infile="$name$cut.fasta";
 
-$gene_in = Bio::SeqIO->new(-file=>'utr5.fasta');
+$gene_in = Bio::SeqIO->new(-file=>'tmp.fasta');
 $gene = $gene_in->next_seq();
 $seq_in = Bio::SeqIO->new(-file=>"../Deep Sequencing/Data/$infile");
 #$seq_in = Bio::SeqIO->new(-file=>'miRNA7+5.fasta');
@@ -34,15 +34,18 @@ while(my $seq = $seq_in->next_seq()) {
 	my @c = split(',', $a[-4]);
 	my $match=$b[1]-$b[0];
 	$id = $seq->display_id;
-	$seq->display_id("$id,$c[0],$c[1]");
+	$seq->display_id("$id,$c[0],$c[1],$b[0],$b[1]");
 	my @last = ($seq->display_id, $seq->length, $match, $score, $seq, $c[0], $c[1]);
-	push @recs, \@last;
+	if (($b[0] < 21) && ($b[1] > 42) ) {
+		push @recs, \@last;
+	}
 }
 
 print "Done\n";
-$mi = Bio::SeqIO->new(-file=>'>>miRNA.fasta', -flush=>1);
+$mi = Bio::SeqIO->new(-file=>'>>db.fasta', -flush=>1);
 @sorted = sort {$a->[3] <=> $b->[3]} @recs;
-foreach $i (0..9) {
+$size = @recs;
+for ($i=0; $i<$size; $i++)  {
 	$newseq = Bio::Seq->new( -display_id => $sorted[$i][0],-seq => $sorted[$i][-3]->subseq($sorted[$i][-2],$sorted[$i][-1]));
 	$mi->write_seq($newseq);
 	foreach $j (@{$sorted[$i]}) {
